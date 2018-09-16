e20cabdbae491d189292f9aaa1ebaa61768a3683
xingjinpeng <xingjinpeng@211.98.71.195>
2018-09-16 16:42:35 +0800

    update

--- application/common/validate/BookValidate.php
+++ application/common/validate/BookValidate.php
@@ -7,13 +7,12 @@ class BookValidate extends Validate{
     protected $rule = [
         'id' => 'require',
         'name' => 'require',
-        'price' => 'require|number'
+        'price' => 'require'
     ];
     protected $message = [
         'id.require' => '图书ID不能为空',
         'name.require' => '书名不能为空',
-        'price.require' => '价格不能为空',
-        'price.number' => '价格必须为数字'
+        'price.require' => '价格不能为空'
     ];
     protected $scene = [
         'save' => ['name', 'price'],
