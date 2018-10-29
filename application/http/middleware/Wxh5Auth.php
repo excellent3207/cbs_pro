b0aeffa9f880bc3651a3cfe4e640135e39d0fb9e
xingjinpeng <xingjinpeng@211.98.71.195>
2018-10-29 15:22:01 +0800

    update

--- application/http/middleware/Wxh5Auth.php
+++ application/http/middleware/Wxh5Auth.php
@@ -15,35 +15,38 @@ class Wxh5Auth {
         });*/
         $wxBiz = new CbsWxBiz();
         $url = 'http://h5.igniter.vip'.$request->url();
-        $user = UserModel::get(1);
-        config('user', $user);
-       /* $code = $request->get('code');
-        $user = session('user');
-        if(empty($user)){
-            $res = $wxBiz->authLogin($url, $code);
-            switch($res['action']){
-                case 'redirect':
-                    header('Location:'.$res['data']);exit;
-                    break;
-                case 'info':
-                    $userModel = new UserModel();
-                    $openid = $res['data']['openid'];
-                    $alias = $res['data']['nickname'];
-                    $face = $res['data']['headimgurl'];
-                    $user = $userModel->get(['wx_openid' => $openid]);
-                    if(empty($user)){
-                        $data = ['wx_openid' => $openid, 'issubscribe' => $res['data']['issubscribe'] ? 1 : 0, 'alias' => $alias,
-                            'face' => $face];
-                        $res = $userModel->save($data);
-                        if(!($res)) throw new \Exception('创建用户失败');
-                        $user = $userModel->get($userModel->id);
-                    }
-                    session('user', serialize($user));
-                    config('user', $user);
-            }
+        if($request->get('token') == 'chubanshe'){
+            $user = UserModel::get(1);
+            config('user', $user);
         }else{
-            config('user', unserialize($user));
-        }*/
+            $code = $request->get('code');
+            $user = session('user');
+            if(empty($user)){
+                $res = $wxBiz->authLogin($url, $code);
+                switch($res['action']){
+                    case 'redirect':
+                        header('Location:'.$res['data']);exit;
+                        break;
+                    case 'info':
+                        $userModel = new UserModel();
+                        $openid = $res['data']['openid'];
+                        $alias = $res['data']['nickname'];
+                        $face = $res['data']['headimgurl'];
+                        $user = $userModel->get(['wx_openid' => $openid]);
+                        if(empty($user)){
+                            $data = ['wx_openid' => $openid, 'issubscribe' => $res['data']['issubscribe'] ? 1 : 0, 'alias' => $alias,
+                                'face' => $face];
+                            $res = $userModel->save($data);
+                            if(!($res)) throw new \Exception('创建用户失败');
+                            $user = $userModel->get($userModel->id);
+                        }
+                        session('user', serialize($user));
+                        config('user', $user);
+                }
+            }else{
+                config('user', unserialize($user));
+            }
+        }
         config('wxConfig', $wxBiz->createJsConfig($url));
         return $next($request);
     }
